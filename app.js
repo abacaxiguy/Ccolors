b531f440c876809bff108fa64983081bc6da8ea7
abacaxiguy <jlucasoc@hotmail.com>
2020-04-28 18:15:55 -0300

    Adding contrast changing

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
+const adjustBtn = document.querySelectorAll(".adjust");
+const lockBtn = document.querySelectorAll(".lock");
 let initialColors;
 
 // Functions
@@ -34,10 +36,29 @@ function randomColors() {
         // Add the color to the bg
         div.style.backgroundColor = randomColor;
         hexText.textContent = randomColor;
+
+        // Check for contrast
+        checkTextContrast(randomColor, [hexText, adjustBtn[i], lockBtn[i]]);
     });
+}
+
+// Change the color of HEX text
+function checkTextContrast(color, text) {
+    const luminance = chroma(color).luminance();
+    for (i of text) {
+        if (luminance > 0.5) {
+            i.style.color = "rgb(35, 35, 35)";
+        } else {
+            i.style.color = "white";
+        }
+    }
 }
 
 // Event listeners
 
 document.addEventListener("load", randomColors());
+document.addEventListener("keypress", function (e) {
+    e.preventDefault();
+    if (e.keyCode == 32) randomColors();
+});
 generateBtn.addEventListener("click", randomColors);
